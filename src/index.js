8025e5f7fa35c60d3e43dcaad19ca7e102349678
ahayuta <forstudynb@gmail.com>
2023-01-26 01:43:58 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
-}
+  let numberToWords = require('number-to-words');
+  let answer = numberToWords.toWords(number);
+  let re = /-/g;
+  let str = answer;
+  let newstr = str.replace(re, ' ');
+  console.log(newstr);
+  return newstr;
+};
+
+
+// function task(number) {
+//   let numberToWords = require('number-to-words');
+//   let answer = numberToWords.toWords(number);
+//   let re = /-/g;
+//   let str = answer;
+//   let newstr = str.replace(re, ' ');
+//   console.log(newstr);
+// };
+
+// task(567);
+
+
